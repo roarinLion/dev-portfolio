0d24423d7970ab047606142d5a294d55e8b78130
Roarin Lion <roarinlion@pm.me>
2024-06-29 15:04:24 -0700

    Add Logo component with styling - Created a new Logo component that displays 'Logo' text - Added CSS module for Logo styling including media query

---
+++ components/Layout/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './logo.module.css'
+
+function Logo() {
+    return (
+        <div className={classes.logo}>Logo</div>
+    )
+}
+
+export default Logo
